d4a6d0a4d0d58a2c99ab874bcade5d14ae29bafd
Elliot Blanchard <git@elliotblanchard.com>
2021-03-30 08:57:39 -0400

    Connected to DB

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const port = 3000
 //Init app
 const app = express()
 
+//Set up DB connection
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017/todoapp'
+
 //Body Parser Middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -17,12 +21,35 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-//Routes
-app.get('/', (req, res, next) => {
-    res.render('index')
+// Create a new MongoClient
+const client = new MongoClient(url)
+
+let Todos = undefined
+
+// Connect to mongodb
+client.connect((err, database) => {
+    console.log('MongoDB Connected...')
+    if(err) throw err
+
+    const todosDB = client.db('todos');
+    Todos = todosDB.collection('todos')
+
+    //Start server
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`)
+    })     
 })
 
-//Start server
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+
+//Routes
+app.get('/', (req, res, next) => {
+    Todos.find({}).toArray((err, todos) => {
+        if(err) {
+            return console.log(err)
+        }
+        console.log(todos)
+        res.render('index',{
+            todos: todos
+        })
+    })
 })
